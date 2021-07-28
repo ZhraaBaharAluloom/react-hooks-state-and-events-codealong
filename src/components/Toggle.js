b18df3e69738b2cf1bc445934f41e5739d940a95
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-07-27 17:42:51 -0700

    hooks

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  const handleClick = () => {
+    setIsOn(!isOn);
+  };
+
+  return (
+    <button
+      style={{ background: isOn ? "white" : "red" }}
+      onClick={handleClick}
+    >
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
